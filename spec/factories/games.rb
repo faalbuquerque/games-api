416b427e0b2d69d78d0d2febd12ef690005ac7bd
Fabiane Albuquerque <bii.nett@gmail.com>
2021-08-26 17:48:00 -0300

    Add Rubocop

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :game do
-    name { "MyString" }
-    description { "MyString" }
-    genre { "MyString" }
+    name { 'MyString' }
+    description { 'MyString' }
+    genre { 'MyString' }
     grade { 1 }
   end
 end
